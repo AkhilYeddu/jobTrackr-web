4e6a804dc3fbbcce28cb33b97a770d73d4df9864
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-11 00:01:12 +0530

    style : navbar changes

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -4,14 +4,14 @@ const DashBoard = () => {
 
         <div className="applied m-5">
             <h3 className="font-semibold m-3">Applied - 3</h3>
-            <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl transition-all duration-300 hover:-translate-y-2 cursor-pointer">
+            <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl transition-all duration-300 hover:-translate-y-2 ">
                 <div className="card-body">
                     <h2 className="card-title font-bold font-bold text-xl">Razorpay</h2>
                     <p>SDE intern</p>
                 <div className="justify-end card-actions">
                     <details className="dropdown ">
-                        <summary className="btn m-1 btn-primary border rounded-xl">open or close</summary>
-                        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <summary className="btn m-1 btn-primary border rounded-xl cursor-pointer  hover:bg-white hover:text-black">open or close</summary>
+                        <ul className="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
                             <li><a>Item 1</a></li>
                             <li><a>Item 2</a></li>
                         </ul>
@@ -24,14 +24,14 @@ const DashBoard = () => {
     </div>
     <div className="oa-screening m-5">
         <h3 className="font-semibold m-3">OA / Screening - 1</h3>
-        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl transition-all duration-300 hover:-translate-y-2 cursor-pointer">
+        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl transition-all duration-300 hover:-translate-y-2 ">
         <div className="card-body">
             <h2 className="card-title font-bold text-xl">Swiggy</h2>
             <p>Backend intern</p>
             <div className="justify-end card-actions">
                     <details className="dropdown ">
-                        <summary className="btn m-1 btn-primary border rounded-xl">open or close</summary>
-                        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <summary className="btn m-1 btn-primary border rounded-xl cursor-pointer  hover:bg-white hover:text-black">open or close</summary>
+                        <ul className="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
                             <li><a>Item 1</a></li>
                             <li><a>Item 2</a></li>
                         </ul>
@@ -43,14 +43,14 @@ const DashBoard = () => {
 
     <div className="interview m-5">
         <h3 className="font-semibold m-3">Interview - 2</h3>
-        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl border-1 border-blue-400 transition-all duration-300 hover:-translate-y-2 cursor-pointer">
+        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl border-1 border-blue-400 transition-all duration-300 hover:-translate-y-2 ">
         <div className="card-body">
             <h2 className="card-title text-xl">Dunzo</h2>
             <p className="text-blue-400 font-bold">Tomorrow, 4pm</p>
             <div className="justify-end card-actions">
                     <details className="dropdown ">
-                <summary className="btn m-1 btn-primary border rounded-xl">open or close</summary>
-                <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                <summary className="btn m-1 btn-primary border rounded-xl cursor-pointer hover:bg-white hover:text-black">open or close</summary>
+                <ul className="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
                     <li><a>Item 1</a></li>
                     <li><a>Item 2</a></li>
                 </ul>
@@ -62,14 +62,14 @@ const DashBoard = () => {
 
     <div className="offer m-5">
         <h3 className="font-semibold m-3">Offer - 1</h3>
-        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl rounded-xl border-1 border-green-400 bg-green-900 transition-all duration-300 hover:-translate-y-2 cursor-pointer">
+        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl rounded-xl border-1 border-green-400 bg-green-900 transition-all duration-300 hover:-translate-y-2">
         <div className="card-body">
             <h2 className="card-title text-green-400 font-bold text-xl">Meesho!</h2>
             <p className="text-green-400 font-bold ">Offer Received</p>
             <div className="justify-end card-actions">
                     <details className="dropdown ">
-                <summary className="btn m-1 btn-primar border rounded-xl btn-disabled">open or close</summary>
-                <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                <summary className="btn m-1 btn-primar border rounded-xl btn-disabled cursor-pointer">open or close</summary>
+                <ul className="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
                     <li><a>Item 1</a></li>
                     <li><a>Item 2</a></li>
                 </ul>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,8 @@ const NavBar = () => {
         <a className="btn btn-ghost text-xl">JobTrackr.</a>
     </div>
     <div className="flex gap-2 p-2">
-        <div className="dropdown dropdown-end">
+        <div className="dropdown dropdown-end flex">
+            <h3 className="m-3">Welcome, User!</h3>
         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar ">
             <div className="w-10 rounded-full">
             <img
@@ -18,11 +19,11 @@ const NavBar = () => {
         </div>
         <ul
             tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            className="menu menu-sm dropdown-content bg-base-300 border rounded-xl border-0 rounded-box z-1 mt-3 w-52 p-2 shadow">
             <li>
             <a className="justify-between">
                 Profile
-                <span className="badge">New</span>
+                <span className="badge border rounded-4xl">New</span>
             </a>
             </li>
             <li><a>Settings</a></li>
