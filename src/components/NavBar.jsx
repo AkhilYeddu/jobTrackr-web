681f6b0b0de244fd05a8f1a6705e579cd39d41f0
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-11 00:26:19 +0530

    style : fixed dropdown covering up the welcome message

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,11 +5,12 @@ const NavBar = () => {
     <>
             <div className="navbar bg-base-200 shadow-sm">
     <div className="flex-1">
-        <a className="btn btn-ghost text-xl">JobTrackr.</a>
+        <a className="btn btn-ghost text-2xl font-extrabold">JobTrackr.</a>
     </div>
-    <div className="flex gap-2 p-2">
-        <div className="dropdown dropdown-end flex">
-            <h3 className="m-3">Welcome, User!</h3>
+    <div className="flex gap-3 p-2 items-center">
+        <h3 className="m-3 text-xl">Welcome, User!</h3>
+        <div className="dropdown dropdown-end">
+            
         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar ">
             <div className="w-10 rounded-full">
             <img
