61420ab6448b9eec33cc69f7f48cb45e0e9cfee0
AkhilYeddu <akhilkiu10@gmail.com>
2026-07-10 23:52:08 +0530

    feat : added hover effect for the cards

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -3,11 +3,11 @@ const DashBoard = () => {
     <div className="dashboard flex">
 
         <div className="applied m-5">
-            <h3 className="font-semibold m-2">Applied - 3</h3>
-            <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl">
+            <h3 className="font-semibold m-3">Applied - 3</h3>
+            <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl transition-all duration-300 hover:-translate-y-2 cursor-pointer">
                 <div className="card-body">
-                    <h2 className="card-title">Small Card</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <h2 className="card-title font-bold font-bold text-xl">Razorpay</h2>
+                    <p>SDE intern</p>
                 <div className="justify-end card-actions">
                     <details className="dropdown ">
                         <summary className="btn m-1 btn-primary border rounded-xl">open or close</summary>
@@ -23,11 +23,11 @@ const DashBoard = () => {
 
     </div>
     <div className="oa-screening m-5">
-        <h3 className="font-semibold m-2">OA / Screening - 1</h3>
-        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl">
+        <h3 className="font-semibold m-3">OA / Screening - 1</h3>
+        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl transition-all duration-300 hover:-translate-y-2 cursor-pointer">
         <div className="card-body">
-            <h2 className="card-title">Small Card</h2>
-            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <h2 className="card-title font-bold text-xl">Swiggy</h2>
+            <p>Backend intern</p>
             <div className="justify-end card-actions">
                     <details className="dropdown ">
                         <summary className="btn m-1 btn-primary border rounded-xl">open or close</summary>
@@ -42,10 +42,10 @@ const DashBoard = () => {
     </div>
 
     <div className="interview m-5">
-        <h3 className="font-semibold m-2">Interview - 2</h3>
-        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl border-1 border-blue-400">
+        <h3 className="font-semibold m-3">Interview - 2</h3>
+        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl border-1 border-blue-400 transition-all duration-300 hover:-translate-y-2 cursor-pointer">
         <div className="card-body">
-            <h2 className="card-title text-xl">Small Card</h2>
+            <h2 className="card-title text-xl">Dunzo</h2>
             <p className="text-blue-400 font-bold">Tomorrow, 4pm</p>
             <div className="justify-end card-actions">
                     <details className="dropdown ">
@@ -61,14 +61,14 @@ const DashBoard = () => {
     </div>
 
     <div className="offer m-5">
-        <h3 className="font-semibold m-2">Offer - 1</h3>
-        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl rounded-xl border-1 border-green-400 bg-green-900">
+        <h3 className="font-semibold m-3">Offer - 1</h3>
+        <div className="card w-96 bg-base-300 card-sm shadow-sm rounded-xl rounded-xl border-1 border-green-400 bg-green-900 transition-all duration-300 hover:-translate-y-2 cursor-pointer">
         <div className="card-body">
             <h2 className="card-title text-green-400 font-bold text-xl">Meesho!</h2>
             <p className="text-green-400 font-bold ">Offer Received</p>
             <div className="justify-end card-actions">
                     <details className="dropdown ">
-                <summary className="btn m-1 btn-primar border rounded-xl">open or close</summary>
+                <summary className="btn m-1 btn-primar border rounded-xl btn-disabled">open or close</summary>
                 <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
                     <li><a>Item 1</a></li>
                     <li><a>Item 2</a></li>
